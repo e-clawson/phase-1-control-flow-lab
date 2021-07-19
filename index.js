5ed68a4b7235d0fdab8142b089bc888ffdd4832a
Elizabeth Clawson <emclawson1@gmail.com>
2021-07-19 17:05:52 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+    return 'This one is on me!';
+    console.log('firstCondition')
+  }
+  else if (2000 <= someNumber && 2500 >= someNumber) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someNumber > 2500); {
+    return 'No can do.';
+  }
+
+}
+scuberGreetingForFeet(199); 
+
+
+function ternaryCheckCity(cityMessage){
+  const allowedCity = 'NYC';
+  const myResult = (allowedCity == cityMessage) ? 'Ok, sounds good.' : 'No go.';
+
+ return myResult;
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someResponse){
+if (someResponse === 'generous') {
+  return 'Thank you so much.'
+}
+ else if (someResponse === 'not as generous'){
+   return 'Thank you.'
+ }
+ else {
+   return 'Bye.'
+ }
+
 }
